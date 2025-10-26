e0348ef3f91582f862f46a46862e20754ebc5ad9
Leo <leo@oliver.nz>
2025-10-26 21:43:34 +1300

    added way to load database file

--- progress.py
+++ progress.py
@@ -618,6 +618,26 @@ class SetRequestStatusCommandHandler(object):
         self._ui_services.display_info("Status set to: %s" % status, "Progress Status Updated")
         self._visible_items.display() # Refresh the table UI
 
+
+class LoadDatabaseCommand(object):
+    def __init__(self, path):
+        self.path = path
+
+
+class LoadDatabaseCommandHandler(object):
+    def __init__(self, persistence, visible_items, visible_path_patterns):
+        self._persistence = persistence
+        self._visible_items = visible_items
+        self._visible_path_patterns = visible_path_patterns
+
+    def handle(self, command):
+        if self._persistence.load_from_path(command.path):
+            self._visible_items.display()
+            self._visible_path_patterns.display()
+            return True
+        return False
+
+
 class Application(object):
     ACTION_TOOLS = ['Intruder', 'Repeater', 'Scanner']
     ITEM_STATUSES = ['New', 'In progress', 'Done', 'AuthTested', 'NA', 'Blocked']
@@ -715,6 +735,11 @@ class Application(object):
             SetSelectedItemPropertiesCommand.__name__: SetSelectedItemPropertiesCommandHandler(
                 selected_items,
                 visible_items
+            ),
+            LoadDatabaseCommand.__name__: LoadDatabaseCommandHandler(
+                persistence,
+                visible_items,
+                visible_path_patterns
             ),
             SetRequestStatusCommand.__name__: SetRequestStatusCommandHandler(
                 duplicate_items,
@@ -1071,14 +1096,19 @@ class DatabasePanel(JPanel, ActionListener):
         self._text_field = None
 
     def actionPerformed(self, event):
+        action_command = event.getActionCommand()
         database_path = UIHelpers.choose_file()
         if database_path:
-            if Application.get_instance().execute(SetDomainDictValueCommand(
-                SetDomainDictValueCommand.TYPE_PERSISTENCE,
-                'database_path',
-                database_path
-            )): #
-                self._text_field.setText(database_path)
+            if action_command == "save_as":
+                if Application.get_instance().execute(SetDomainDictValueCommand(
+                    SetDomainDictValueCommand.TYPE_PERSISTENCE,
+                    'database_path',
+                    database_path
+                )):
+                    self._text_field.setText(database_path)
+            elif action_command == "load":
+                if Application.get_instance().execute(LoadDatabaseCommand(database_path)):
+                    self._text_field.setText(database_path)
 
     def display(self, values):
         self._prepare_components(values)
@@ -1089,9 +1119,14 @@ class DatabasePanel(JPanel, ActionListener):
         self._text_field.setEditable(False)
         self._text_field.setText(values['database_path'])
         self.add(self._text_field)
-        button = JButton('Save as...')
-        button.addActionListener(self)
-        self.add(button)
+        save_button = JButton('Save as...')
+        save_button.setActionCommand("save_as")
+        save_button.addActionListener(self)
+        self.add(save_button)
+        load_button = JButton('Load...')
+        load_button.setActionCommand("load")
+        load_button.addActionListener(self)
+        self.add(load_button)
 
 
 class DeleteSelectedObjectsCommand(object):
@@ -2413,12 +2448,10 @@ class Persistence(DomainDict):
     def __init__(self, database, item_repository, path_pattern_repository, ui_services, value_repository):
         super(Persistence, self).__init__(value_repository)
         self._database = database
-        self._repositories = [
-            item_repository,
-            path_pattern_repository,
-        ]
+        self._item_repository = item_repository
+        self._path_pattern_repository = path_pattern_repository
         self._ui_services = ui_services
-        self._load()
+        self.load()
 
     # DomainDict
     def _get_default_values(self):
@@ -2426,53 +2459,49 @@ class Persistence(DomainDict):
             'database_path': '',
         }
 
-    # business logic
     def set_value(self, key, value):
-        if self._persist(value):
+        if key == 'database_path':
+            if self._database.is_connected():
+                self._database.disconnect()
+            if value:
+                self._database.connect(value)
+                if self._database.is_connected():
+                    self._item_repository.init_persistence()
+                    self._path_pattern_repository.init_persistence()
+                else:
+                    self._ui_services.display_error('Could not connect to database.')
+                    value = ''
             super(Persistence, self).set_value(key, value)
-            return True
-        return False
+            return self._database.is_connected() or not value
+        super(Persistence, self).set_value(key, value)
 
-    def _load(self):
-        if self._values['database_path']:
-            self._database.connect(self._values['database_path'])
-            for repository in self._repositories:
-                repository.load()
+    def load(self):
+        database_path = self._values['database_path']
+        if database_path:
+            self._database.connect(database_path)
+            if self._database.is_connected():
+                self._item_repository.load()
+                self._path_pattern_repository.load()
+            else:
+                self._ui_services.display_error('Could not connect to database.')
 
-    def _persist(self, database_path):
-        if (
-            self._is_driver_loaded() and
-            self._prepare_database_file(database_path) and
-            self._persist_repositories(database_path)
-        ):
-            return True
-        return False
-
-    def _is_driver_loaded(self):
-        if not self._database.is_driver_loaded():
-            self._ui_services.display_error(
-                '%s driver not found (see "Requirements" on https://github.com/dariusztytko/progress-burp)' % self._database.get_driver_name())
-            return False
-        return True
-
-    def _prepare_database_file(self, database_path):
-        try:
-            if os.path.exists(database_path):
-                if not self._ui_services.confirm('File already exists. Are you sure you want to replace it?'):
-                    return False
-            with open(database_path, 'ab') as f:
-                f.truncate(0)
-            return True
-        except IOError as e:
-            self._ui_services.display_error(str(e))
-
-    def _persist_repositories(self, database_path):
+    def load_from_path(self, database_path):
         if self._database.is_connected():
             self._database.disconnect()
+
         self._database.connect(database_path)
-        for repository in self._repositories:
-            repository.init_persistence()
-        return True
+
+        if self._database.is_connected():
+            self._item_repository.load()
+            self._path_pattern_repository.load()
+            # Also need to update the path in the settings
+            super(Persistence, self).set_value('database_path', database_path)
+            return True
+        else:
+            self._ui_services.display_error('Could not connect to database.')
+            # On failure, clear the path
+            super(Persistence, self).set_value('database_path', '')
+            return False
 
 
 class PreAnalyzeValidator(DomainDictWithLock):
